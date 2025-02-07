729fb8d257c089325cb1397f7031af769f7d318f
Samodra <samodraland@yahoo.com>
2025-02-07 10:07:01 +0700

    simple html tempate
    html template for html() rendering function

---
+++ view/view-employees.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee list</title>
+    <style>
+        table{
+            border: 1px solid #eaeaea;
+            border-collapse: collapse;
+            width: 80%;
+            margin: auto
+        }
+        td,th{
+            text-align: left;
+            padding: 8px;
+            border: 1px solid #eaeaea;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                print_r($_POST);
+                foreach($_POST as $key => $value){
+            ?>
+            <tr>
+                <td><?=$value["FirstName"]?> <?=$value["LastName"]?></td>
+                <td><?=$value["Email"]?></td>
+                <td><?=ucfirst(strtolower($value["Status"]))?></td>
+            </tr>
+            <?php
+                }
+            ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
